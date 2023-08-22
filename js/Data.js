0c448b001558191280c0e26c77b098035ce36a72
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2023-08-22 12:05:57 -0300

    .

--- js/Data.js
+++ js/Data.js
@@ -2,21 +2,21 @@ let Data = [
     {
         id: 1,
         title: "WSMaps",
-        text: "Algum texto sobre o WSMaps",
+        text: "Made with React.js and maps based on the Google Maps API, WSMaps is a user interface created with collaborative cartography, which seeks to show the reality of some neighborhoods in Pará, through the perspective of its residents.",
         imgDir: "./image/wsmaps.png",
         href: "https://imfernandes23.github.io/wsmaps/"
     },
     {
         id: 2,
         title: "Admin Dashboard",
-        text: "Algum texto sobre o Admin DashBoard",
+        text: "made with react.js, vite and typescript, this project is concept style for an online sales tracking application, working with lists of users and products, using data charts with Recart.js.",
         imgDir: "./image/admin.png",
-        href: "#"
+        href: "https://imfernandes23.github.io/admin-dashboard/"
     },
     {
         id: 3,
         title: "Portifólio",
-        text: "Algum texto sobre o Portifólio.",
+        text: "Made with Matte.js to simulate physics with JavaScript, and Paper.js to create drawings, this page is my personal portfolio, where I intend to add new projects.",
         imgDir: "./image/portifolio.png",
         href: "#"
     },
